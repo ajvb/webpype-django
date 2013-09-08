033c50eca6ac638fe4f39773d2f680061aa7beb7
AJ Bahnken <aj@ajvb.me>
2013-09-07 21:35:02 -0700

    Implemented more of the webpipe spec into WebPypeMixin

--- webpype_django/views.py
+++ webpype_django/views.py
@@ -1,19 +1,77 @@
 import json
+from exceptions import NotImplementedError
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
-class WebPypeMixin(object):
+from webpype.client import WebPypeClient
+
+webpipe = WebPypeClient()
+
+class WebPypeMixin(View):
     '''
     A Django Mixin for making a view a WebPipe
 
     '''
     http_method_names = ['post', 'options']
     block_definition = None
+    is_block = False
+    is_trigger = False
+    is_action = False
 
-    def post(self, *args, **kwargs):
-        pass
+    def __init__(self, **kwargs):
+        if not (self.is_block or self.is_trigger or self.is_action):
+            raise NotImplementedError()
 
-    def options(self, *args, **kwargs):
-        data = json.dumps(self.block_definition)
+        if not self.block_definition:
+            raise NotImplementedError()
+
+        return super(WebPypeMixin, self).__init__(**kwargs)
+
+    def _parse_inputs(self, inputs):
+        resp = webpipe._validate_input(inputs)
+        return json.dumps(resp)
+
+    def block(self, inputs):
+        raise NotImplementedError()
+
+    def trigger(self):
+        raise NotImplementedError()
+
+    def action(self, inputs):
+        raise NotImplementedError()
+
+    def post(self, request, *args, **kwargs):
+        if not self.is_trigger:
+            #TODO this sucks
+            inputs = request.POST.items()[0][0]
+            if inputs:
+                inputs = self._parse_inputs(inputs)
+            else:
+                return HttpResponseBadRequest(*args, **kwargs)
+
+        if self.is_block:
+            output = self.block(inputs)
+        elif self.is_action:
+            self.action(inputs)
+        elif self.is_trigger:
+            output = self.trigger()
+        else:
+            raise NotImplementedError()
+
         kwargs['content_type'] = 'application/json'
-        return HttpResponse(data, **kwargs)
+        if self.is_action:
+            return HttpResponse('OK', *args, **kwargs)
+        else:
+            return HttpResponse(output, *args, **kwargs)
+
+    def options(self, request, *args, **kwargs):
+        output = json.dumps(self.block_definition)
+        kwargs['content_type'] = 'application/json'
+        return HttpResponse(output, *args, **kwargs)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(WebPypeMixin, self).dispatch(*args, **kwargs)
